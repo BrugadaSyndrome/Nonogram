a41e5f36b5275bff1730099963b83b55f4ec8d8d
Coby Johnson <i2c2.caj@gmail.com>
2017-01-16 13:36:51 -0700

    more work on templates and nonogram css

--- router.go
+++ router.go
@@ -3,9 +3,9 @@ package main
 import "net/http"
 
 type indexData struct {
-	Title   string
-	Master  string
 	Log     []string
+	Master  master
+	Title   string
 	Workers []worker
 }
 
@@ -16,9 +16,30 @@ type logData struct {
 func index(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(http.StatusOK)
 
+	n := nonogram{}
+	n.Height = 5
+	n.Width = 5
+	n.Board = make([][]mark, n.Height)
+	for i := 0; i < n.Height; i++ {
+		n.Board[i] = make([]mark, n.Width)
+	}
+
+	m := master{}
+	m.Puzzle = n
+
+	w1 := worker{}
+	w1.ID = 1
+	w1.Puzzle = n
+
+	w2 := worker{}
+	w2.ID = 2
+	w2.Puzzle = n
+
 	context := indexData{
-		Title: "Nonogram Solver",
-		Log:   []string{"[w1] log", "[w2] log", "[m] log"},
+		Log:     []string{"msg 1", "msg 2", "msg 3"},
+		Master:  m,
+		Title:   "Nonogram Solver",
+		Workers: []worker{w1, w2},
 	}
 
 	err := templates.Execute(w, context)
