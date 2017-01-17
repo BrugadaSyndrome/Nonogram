9ed1f515be2240731d89d9604521afe8b6896fc4
Coby Johnson <i2c2.caj@gmail.com>
2017-01-17 16:15:34 -0700

    trying to get the row hints displayed

--- nonogram.go
+++ nonogram.go
@@ -25,8 +25,51 @@ type move struct {
 // Hints stores the hints needed to solve the puzzle
 // Width number of columns in the puzzle
 type nonogram struct {
-	Board  [][]mark
-	Height int
-	Hints  [][]int
-	Width  int
+	Board       [][]mark
+	ColumnHints [][]int
+	Height      int
+	RowHints    [][]int
+	Width       int
+}
+
+func sampleNonogram() (n nonogram) {
+	height := 5
+	width := 5
+
+	n.Board = make([][]mark, height)
+	for i := 0; i < height; i++ {
+		n.Board[i] = make([]mark, width)
+	}
+	//fmt.Println(n.Board)
+	n.ColumnHints = make([][]int, width)
+	n.ColumnHints[0] = []int{5}
+	n.ColumnHints[1] = []int{1, 1}
+	n.ColumnHints[2] = []int{1}
+	n.ColumnHints[3] = []int{1, 1}
+	n.ColumnHints[4] = []int{5}
+	//fmt.Println(n.ColumnHints)
+	/*
+		n.ColumnHints = make([][]int, width)
+		for i := 0; i < width; i++ {
+			// need hints from user...
+			n.ColumnHints[i] = make([]int, 0)
+		}
+	*/
+	n.Height = 5
+	n.RowHints = make([][]int, height)
+	n.RowHints[0] = []int{1, 1}
+	n.RowHints[1] = []int{2, 2}
+	n.RowHints[2] = []int{5}
+	n.RowHints[3] = []int{2, 2}
+	n.RowHints[4] = []int{1, 1}
+	/*
+		n.RowHints = make([][]int, height)
+		for i := 0; i < height; i++ {
+			// need hints from user...
+			n.RowHints[i] = make([]int, 0)
+		}
+	*/
+	//fmt.Println(n.RowHints)
+	n.Width = 5
+	return
 }
